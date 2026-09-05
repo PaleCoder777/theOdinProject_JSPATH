ced592058c7ae20103722336037fda130e9b5f7f
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-09-05 11:29:27 -0400

    complete organizing code with objects lesson

--- javascript/organize-js-code/script.js
+++ javascript/organize-js-code/script.js
@@ -1,3 +1,38 @@
 // File Connection Verification
 let test = ("The JS page is conncted");
 console.log(test);
+
+// Organize Code with Objects Lesson
+
+    /* TOP Overview Lesson
+    const myObject = {
+        property: "Value!",
+        otherProperty: 77,
+        "obnoxious property": function() {
+            console.log("Function called, I am obnoxious function");
+        },
+    };
+
+
+    const car = {
+        make: "Volkswagen",
+        model: "Golf",
+        year: 2026,
+        color: "blue",
+        priceUSD: 40000,
+        // a method is just a function assigned to a property
+        applyDiscount: function(discountPercentage) {
+            const multiplier = 1 - discountPercentage / 100;
+            this.priceUSD *= multiplier;
+            console.log(this.priceUSD);
+        },
+        // shorthand way to add method to object literal
+        getSummary() {
+            return `${this.year} ${this.make} ${this.model} in ${this.color}, priced at $${this.priceUSD} (USD).`;
+        },
+    };
+    */
+
+// Object Constructors
+
+    // Lesson
